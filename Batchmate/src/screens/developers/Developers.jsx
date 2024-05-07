cf84daa1686dd63b491b89b712d1143b4c82beef
Naman <thisisnamansaini@gmail.com>
2024-05-07 23:06:14 +0530

    rem searchBar and fix dev card width

--- Batchmate/src/screens/developers/Developers.jsx
+++ Batchmate/src/screens/developers/Developers.jsx
@@ -7,7 +7,7 @@ const Developers = () => {
         <>
             <Navbar />
             <h1 className='text-2xl p-1 font-extrabold text-center bg-orange-400 rounded-3xl m-3 shadow-md'>Meet Our Team</h1>
-            <div className='w-screen h-screen p-4 flex gap-4 justify-around flex-wrap'>
+            <div className='w-screen h-screen p-3 flex gap-3 justify-evenly flex-wrap'>
                 <DevelopersData name="HANISH" role="Developer" insta="its_haniish" linkedin="its-hanish" twitter="its.haniish_" />
                 <DevelopersData name="NAMAN" role="Developer" insta="pankha.hu" linkedin="programmerxd" twitter="dotpxd" />
                 <DevelopersData name="JASIKA" role="Designer" insta="its.jasika72" />
