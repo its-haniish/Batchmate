18c784bf650d2a9c3e7087c517d0dba9b59b7c7b
toxiic-me <iamtoxiicman@gmail.com>
2024-05-07 23:04:33 +0530

    roll number api fixed

--- Server/controllers/getUserDetails.js
+++ Server/controllers/getUserDetails.js
@@ -4,7 +4,7 @@ const getUserDetails = async (req, res) => {
 
     try {
         let response = await Students.findById(req.user.id)
-        const { name, email, image, _id } = response
+        const { name, email, image, _id, rollNo } = response
         res.status(200).json({ name, email, image, _id });
     } catch (error) {
         res.status(500).json({ error: "Server Internal Error" })
