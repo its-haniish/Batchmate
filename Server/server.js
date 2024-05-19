ee85d0b7c234e5b83b9612fb70a9769336c71e95
toxiic-me <iamtoxiicman@gmail.com>
2024-05-19 12:37:35 +0530

    self ping added to stop server from sleeping

--- Server/server.js
+++ Server/server.js
@@ -2,12 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const path = require('path'); // Import the path module
+const path = require('path');
 
-
-const PORT = process.env.PORT || 8080
-const connectDB = require('./functions/connectDB.js')
-const routes = require('./routes/routes.js')
+const PORT = process.env.PORT || 8080;
+const connectDB = require('./functions/connectDB.js');
+const routes = require('./routes/routes.js');
 
 app.use(express.json());
 app.use(cors());
@@ -15,14 +14,37 @@ app.use(cors());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/', routes);
 
+// Define the /ping route
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
+// Function to ping the server periodically
+const startSelfPing = () => {
+    console.log('Starting self-ping function...');
+    const interval = 14 * 60 * 1000; // 14 minutes in milliseconds
+
+    const pingServer = async () => {
+        try {
+            const fetch = (await import('node-fetch')).default;
+            const response = await fetch(`https://batchmate-server.onrender.com/ping`);
+            if (response.ok) {
+                console.log('Ping successful');
+            } else {
+                console.error('Ping failed', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error pinging server:', error);
+        }
+    };
+
+    setInterval(pingServer, interval);
+};
 
 // calling the connectDB function and listening server in then block
 connectDB(process.env.DATABASE_URI).then(() => {
-    // listening server
     app.listen(PORT, () => {
         console.log('Server is running on port:', PORT);
+        startSelfPing(); // Start the self-ping function
     });
 });
-
-
-
