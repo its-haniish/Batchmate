5caa78e50252a1021a509661ed53ba05e95d0154
toxiic-me <iamtoxiicman@gmail.com>
2024-05-21 11:32:10 +0530

    updated server json limit to 50mb

--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 const connectDB = require('./functions/connectDB.js');
 const routes = require('./routes/routes.js');
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
