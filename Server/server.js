0c6dde07be58c8fc1d0c8665a1ae85ab3603ea34
Hanish Kumar <142662543+its-haniish@users.noreply.github.com>
2024-07-16 22:05:33 +0530

    Removed ping

--- Server/server.js
+++ Server/server.js
@@ -14,37 +14,9 @@ app.use(cors());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/', routes);
 
-// Define the /ping route
-app.get('/ping', (req, res) => {
-    res.send('pong');
-});
-
-// Function to ping the server periodically
-const startSelfPing = () => {
-    console.log('Starting self-ping function...');
-    const interval = 14 * 60 * 1000; // 14 minutes in milliseconds
-
-    const pingServer = async () => {
-        try {
-            const fetch = (await import('node-fetch')).default;
-            const response = await fetch(`https://batchmate-server.onrender.com/ping`);
-            if (response.ok) {
-                console.log('Ping successful');
-            } else {
-                console.error('Ping failed', response.statusText);
-            }
-        } catch (error) {
-            console.error('Error pinging server:', error);
-        }
-    };
-
-    setInterval(pingServer, interval);
-};
-
 // calling the connectDB function and listening server in then block
 connectDB(process.env.DATABASE_URI).then(() => {
     app.listen(PORT, () => {
         console.log('Server is running on port:', PORT);
-        startSelfPing(); // Start the self-ping function
     });
 });
